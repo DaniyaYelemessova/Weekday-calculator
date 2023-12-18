14fd409d6a23410b3605b8217dea6804b994496b
Daniya Yelemessova <daniya@collingsmail.com>
2023-12-18 13:47:34 -0500

    Add changes

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -6,3 +6,10 @@ describe('Calculator', () => {
     expect(calculateDay.calculate(2023, 12, 18)).toMatch('Monday');
   });
 });
+
+describe('Calculator', () => {
+  test('should not calculate when date is not valid', () => {
+    const calculateDay = new Calculator();
+    expect(calculateDay.calculate(2023, 11, 31)).toBe('Cannot calculate: INVALID DATE');
+  });
+});
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,27 +1,38 @@
 export default class Calculator {
-  constructor() {
-    this.daysOfWeek = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday", // reset to this when submit is clicked twice 
-      "Friday",
-      "Saturday",
-    ];
-  }
+    constructor() {
+        this.daysOfWeek = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday", 
+            "Friday",
+            "Saturday",
+        ];
+    }
 
-  calculate(year, month, day) {
-    const inputValue = new Date(year, month - 1, day); // because Jan is set to 0
-    // using .getDay() will render numbers from 0 to 6
-    // ex) 0 would be: this.daysOfWeek[0] === 'Sunday'
-    // so we have to make the value from .getDay() as an index
-    const dayOfWeek = this.daysOfWeek[inputValue.getDay()];
-    return dayOfWeek;
-  }
+    isValidDate(year, month, day) {
+        const inputValue = new Date(year, month - 1, day); // because Jan is set to 0
+        return (
+            inputValue.getFullYear() === year &&
+            inputValue.getMonth() === month - 1 &&
+            inputValue.getDate() === day
+            // 2023-11-31 > not in their data???
+        );
+    }
+
+    calculate(year, month, day) {
+        // call 'isValidDate' function to check validity
+        if (!this.isValidDate(year, month, day)) {
+            return 'Cannot calculate: INVALID DATE';
+        }
+        // if valid, proceed to next: 
+        const inputValue = new Date(year, month - 1, day); // because Jan is set to 0
+        // using .getDay() will render numbers from 0 to 6
+        // ex) 0 would be: this.daysOfWeek[0] === 'Sunday'
+        // value from .getDay() = an index
+        const dayOfWeek = this.daysOfWeek[inputValue.getDay()];
+        return dayOfWeek;
+    }
 }
 
-// const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
-// const d = new Date();
-// let day = weekday[d.getDay()];
